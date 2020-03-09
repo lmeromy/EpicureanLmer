7490b4e911c06a9f51e4bb829e2a768f514bb6c7
lmeromy <41759460+lmeromy@users.noreply.github.com>
2020-03-09 07:14:58 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="nav-bar">NavBar</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header-main">Epicurean Lmer</div>
+      <div className="header-sub">Lmer's curated foodie/travel guide</div>
+    </>
+  );
+};
+
+export default Header;
