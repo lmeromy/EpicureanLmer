d58bb175806dd13d58f4420d7a0a98def03b6348
lmeromy <41759460+lmeromy@users.noreply.github.com>
2020-04-21 20:11:40 -0600

    fetch countries from REST api

--- src/Destinations/Destinations.jsx
+++ src/Destinations/Destinations.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../Layout/Layout";
+import { leahCountryInfo } from "./FetchCountries";
+import { SingleCountry } from "./SingleCountry";
+import "./Destinations.scss";
 
 export const Destinations = () => {
+  const [countries, setCountries] = useState([]);
+  leahCountryInfo().then((data) => setCountries(data));
+
   return (
     <>
       <Layout />
       <div className="main-destinations">
         <h1> Here are all my destinations</h1>
+        <div className="country-container">
+          {countries.map((country) => (
+            <SingleCountry
+              key={country.name}
+              name={country.name}
+              flag={country.flag}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
