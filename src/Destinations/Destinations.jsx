c56081b4e155019df140bbe7f4a510e2fda538f5
lmeromy <41759460+lmeromy@users.noreply.github.com>
2020-04-26 11:13:41 -0600

    Add header stuff, add loader animation

--- src/Destinations/Destinations.jsx
+++ src/Destinations/Destinations.jsx
@@ -8,28 +8,34 @@ import "./Destinations.scss";
 export const Destinations = () => {
   const countryPath =
     "https://restcountries.eu/rest/v2/all?fields=name;capital;flag";
-  const [data, loading] = useFetch(countryPath);
+  let [data, loading] = useFetch(countryPath);
   const filteredCountries = leahCountryInfo(data);
 
   return (
     <>
-      <Layout />
-      <div className="main-destinations">
-        <h1> Click on a Flag to see Lmer's recs</h1>
-        {loading ? (
-          "Loading..."
-        ) : (
-          <div className="countries-container">
-            {filteredCountries.map((country) => (
-              <SingleCountry
-                key={country.name}
-                name={country.name}
-                flag={country.flag}
-              />
-            ))}
+      {loading ? (
+        <div className="loader"></div>
+      ) : (
+        <>
+          <Layout />
+          <div className="main-destinations">
+            <h1> Click on a Flag to see Lmer's recs</h1>
+            {loading ? (
+              "Loading..."
+            ) : (
+              <div className="countries-container">
+                {filteredCountries.map((country) => (
+                  <SingleCountry
+                    key={country.name}
+                    name={country.name}
+                    flag={country.flag}
+                  />
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </>
+      )}
     </>
   );
 };
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HomeImage from "../images/undraw_cabin_hkfr.png";
 import "./Header.scss";
 
 const Header = () => {
+  const tempNotice = () => {
+    alert(
+      "Translation is a work-in-progress / La traduction est un travail en cours"
+    );
+  };
   return (
     <div className="header-container">
-      <div className="header-main">
-        <Link to="/">Epicurean Lmer</Link>
+      <div className="home-icon-link">
+        <Link to="/">
+          <img src={HomeImage} alt="Link to Home" />
+        </Link>
       </div>
-      <div className="header-sub">
-        <Link to="/">Lmer's curated foodie/travel guide</Link>
+      <div className="header-main">
+        <div className="header-title">
+          <Link to="/">Epicurean Lmer</Link>
+        </div>
+        <div className="header-sub">
+          <Link to="/">Lmer's curated foodie/travel guide</Link>
+        </div>
+      </div>
+      <div className="home-translate">
+        <button onClick={tempNotice}>English/Francais</button>
       </div>
     </div>
   );
